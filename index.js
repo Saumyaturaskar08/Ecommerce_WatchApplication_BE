a84e3598d86bc0f72b1fafff02249df4cb699c92
Saumya Turaskar <saumya.turaskar@gmail.com>
2025-03-07 18:15:05 +0530

    new

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ let bodyparser=require("body-parser")
 mongoose.connect("mongodb://127.0.0.1:27017/Ecommerce_V").then(()=>{
     console.log("connection ok")
 })
+
+//
+// let corsOptions = {
+//     origin: 'https://singular-muffin-dde045.netlify.app'
+//   }; 
+let corsOptions = {
+  origin: '*',  // Allow all origins temporarily for testing
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true
+};
+
 let app=express()
 app.use(express.json())
 app.use(bodyparser.urlencoded({extended:true}));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.static('./prodimages'))
 app.use("/",userrt)
 
